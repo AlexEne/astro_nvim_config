e4839d7dc8aae36ff82fef92458d7739002bd432
Alexandru Ene <alex.ene0x11@gmail.com>
2024-01-20 15:16:18 +0000

    rainbow delimiters catpuccin

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,19 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
+  {
+    "catppuccin",
+    opts = {
+      integrations = {
+        rainbow_delimiters = true,
+        noice = true,
+        lsp_trouble = true,
+      },
+    },
+  },
+
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
 }
